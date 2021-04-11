960ec6127c49b8a2002aa3a874f2c0785e966783
Mr. Gulab Patel <62597299+gulabpatel@users.noreply.github.com>
2021-04-11 17:31:48 +0530

    Add files via upload

--- Solidity_Basics/mapping.sol
+++ Solidity_Basics/mapping.sol
@@ -1,27 +1,10 @@
-pragma solidity ^0.6.0;
-
-contract MyContract {
-    //Mappings
+pragma solidity ^0.4.24;
     
-    mapping(uint => string) public names;
-    mapping(uint => Book) public books;
-    mapping(address => mapping(uint => Book)) public myMapping;
-    struct Book {
-        string title;
-        string Author;
-    }
     
-    constructor() public {
-        names[1] = "Adam";
-        names[2] = "Gulab";
-        names[3] = "Patel";
+    contract Auction{
+        mapping(address => uint) public bids;
+        
+        function bid() payable public {
+            bids[msg.sender] = msg.value;
+        }
     }
-    
-    function addBook(uint _id, string memory _title, string memory _author) public {
-        books[_id] = Book(_title, _author)  ;      
-    }
-    
-    function addBook(uint _id, string memory _title, string memory _author) public {
-        books[_id] = Book(_title, _author)  ;      
-    }
-}
